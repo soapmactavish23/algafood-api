0f92bc4199540a58d5806b57ddd2e6564fd174f1
Henrick <ricknogueira1231@gmail.com>
2023-11-24 22:48:04 -0300

    2.25

--- src/main/java/com/hkprogrammer/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/hkprogrammer/algafood/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.hkprogrammer.algafood.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +10,18 @@ import com.hkprogrammer.algafood.models.Cliente;
 @Component
 @TipoNotificador(NivelUrgencia.SEM_URGENCIA)
 public class NotificadorEmail implements Notificador {
+	
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
 
 	public void notificar(Cliente cliente, String message) {
+		
+		System.out.println(host);
+		System.out.println(porta);
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), message);
 	}
 	
