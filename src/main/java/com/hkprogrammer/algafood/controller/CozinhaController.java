482fcbd882f6ee95c4037fb59932b4d19c965dda
Henrick <ricknogueira1231@gmail.com>
2024-01-19 19:46:07 -0300

    4.26. Modelando e implementando a exclusão de recursos com DELETE

--- src/main/java/com/hkprogrammer/algafood/controller/CozinhaController.java
+++ src/main/java/com/hkprogrammer/algafood/controller/CozinhaController.java
@@ -3,16 +3,20 @@ package com.hkprogrammer.algafood.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hkprogrammer.algafood.models.Cozinha;
-import com.hkprogrammer.algafood.models.CozinhaXmlWrapper;
 import com.hkprogrammer.algafood.repository.CozinhaRepository;
 
 @RestController
@@ -22,22 +26,49 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository repository;
 
-	@GetMapping(produces = MediaType.APPLICATION_PROBLEM_XML_VALUE)
-	public List<Cozinha> listar() {
-		return repository.findAll();
+	@GetMapping
+	public ResponseEntity<List<Cozinha>> listar() {
+		List<Cozinha> list = repository.findAll();
+		return ResponseEntity.ok(list);
 	}
-
-//	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-//	public CozinhaXmlWrapper listarXML() {
-//		CozinhaXmlWrapper obj = new CozinhaXmlWrapper(repository.findAll());
-//		System.out.println(obj);
-//		return obj;
-//	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 		Optional<Cozinha> cozinha = repository.findById(id);
-		return ResponseEntity.ok(cozinha.get());
+		if (cozinha.isPresent()) {
+			return ResponseEntity.ok(cozinha.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<Cozinha> adicionarCozinha(@RequestBody Cozinha cozinha) {
+		Cozinha objSaved = repository.save(cozinha);
+		return ResponseEntity.status(HttpStatus.CREATED).body(objSaved);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> update(@RequestBody Cozinha obj, @PathVariable Long id) {
+		Cozinha objSaved = buscar(id).getBody();
+		BeanUtils.copyProperties(obj, objSaved, "id");
+		return ResponseEntity.ok(objSaved);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Cozinha> delete(@PathVariable Long id) {
+		try {
+			Cozinha cozinha = repository.buscar(id);
+
+			if (cozinha != null) {
+				repository.deleteById(id);
+				return ResponseEntity.noContent().build();
+			}
+
+			return ResponseEntity.noContent().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
 	}
 
 }
--- src/main/java/com/hkprogrammer/algafood/models/Cozinha.java
+++ src/main/java/com/hkprogrammer/algafood/models/Cozinha.java
@@ -4,13 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Data;
 
 @Data
 @Entity
-@XmlRootElement
 public class Cozinha {
 
 	@Id
