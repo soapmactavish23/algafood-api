41001de9acdf29507f669a9dcc484abd32a4273b
unknown <ricknogueira1231@gmail.com>
2024-01-30 13:35:41 -0300

    5.1. Implementando consultas JPQL em repositórios

--- src/main/java/com/hkprogrammer/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/hkprogrammer/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -17,32 +17,37 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	@Override
 	public List<Cozinha> listar() {
-		return manager.createQuery("from Cozinha", Cozinha.class)
-				.getResultList();
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
-	
+
 	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
-	
+
 	@Transactional
 	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
-	
+
 	@Transactional
 	@Override
 	public void remover(Long cozinhaId) {
 		Cozinha cozinha = buscar(cozinhaId);
-		if(cozinha == null) {
+		if (cozinha == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
 		manager.remove(cozinha);
+	}
+
+	@Override
+	public List<Cozinha> consultarPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome = :nome", Cozinha.class).setParameter("nome", nome)
+				.getResultList();
 	}
 
 }
