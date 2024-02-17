82b0331916afa14f353b820f606150e4f60d6b43
Henrick <ricknogueira1231@gmail.com>
2024-02-17 13:06:28 -0300

    5.17. Implementando Specifications com SDJ

--- src/main/java/com/hkprogrammer/algafood/api/controller/TestController.java
+++ src/main/java/com/hkprogrammer/algafood/api/controller/TestController.java
@@ -13,6 +13,8 @@ import com.hkprogrammer.algafood.domain.models.Cozinha;
 import com.hkprogrammer.algafood.domain.models.Restaurante;
 import com.hkprogrammer.algafood.domain.repository.CozinhaRepository;
 import com.hkprogrammer.algafood.domain.repository.RestauranteRepository;
+import com.hkprogrammer.algafood.infraestructure.repository.spec.RestauranteComFreteGratisSpec;
+import com.hkprogrammer.algafood.infraestructure.repository.spec.RestauranteComNomeSemelhanteSpec;
 
 @RestController
 @RequestMapping("/teste")
@@ -33,6 +35,14 @@ public class TestController {
 	public List<Restaurante> findRestauranteByName(String nome, BigDecimal taxaFreteInicial,
 			BigDecimal taxaFreteFinal) {
 		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}
+	
+	@GetMapping("/restaurantes/com-frete-gratis")
+	public List<Restaurante> restaurantesComFreteGratis(String nome) {
+		var comFreteGratis = new RestauranteComFreteGratisSpec();
+		var comNomeSemelhante = new RestauranteComNomeSemelhanteSpec(nome);
+		
+		return restauranteRepository.findAll(comNomeSemelhante.and(comFreteGratis));
 	}
 
 }
--- src/main/java/com/hkprogrammer/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/hkprogrammer/algafood/domain/repository/RestauranteRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.hkprogrammer.algafood.domain.models.Restaurante;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteInterfaceQueries {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteInterfaceQueries, 
+							JpaSpecificationExecutor<Restaurante> {
 
 	public List<Restaurante> findByTaxaFreteBetween(BigDecimal tx1, BigDecimal tx2);
 	
