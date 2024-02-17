0933517a25c0ed628ff2466c20851da56e0a8074
Henrick <ricknogueira1231@gmail.com>
2024-02-17 20:08:01 -0300

    6.9. Desafio: mapeando relacionamentos muitos-para-muitos

---
+++ src/main/java/com/hkprogrammer/algafood/domain/models/Grupo.java
@@ -0,0 +1,30 @@
+package com.hkprogrammer.algafood.domain.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Grupo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"), inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes = new ArrayList<>();
+	
+}
---
+++ src/main/java/com/hkprogrammer/algafood/domain/models/Usuario.java
@@ -0,0 +1,39 @@
+package com.hkprogrammer.algafood.domain.models;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String email;
+	
+	private String senha;
+	
+	@CreationTimestamp
+	@Column(nullable = false)
+	private LocalDateTime dataCadastro;
+	
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos;
+	
+}
