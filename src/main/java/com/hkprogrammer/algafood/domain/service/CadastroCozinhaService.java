34ee795aab389562833d9fa5b82f1ce7dd53f91e
Henrick <ricknogueira1231@gmail.com>
2024-02-21 13:45:55 -0300

    8.3. Lançando exceções do tipo ResponseStatusException

--- src/main/java/com/hkprogrammer/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/hkprogrammer/algafood/domain/service/CadastroCozinhaService.java
@@ -3,7 +3,9 @@ package com.hkprogrammer.algafood.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.hkprogrammer.algafood.domain.exception.EntidadeEmUsoException;
 import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
@@ -25,7 +27,7 @@ public class CadastroCozinhaService {
 			repository.deleteById(cozinhaId);
 		} catch (EmptyResultDataAccessException e) {
 			String msg = String.format("Não existe um cadastro de cozinha com código %d", cozinhaId);
-			throw new EntidadeNaoEncontradaException(msg);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
 		} catch (DataIntegrityViolationException e) {
 			String msg = String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId);
 			throw new EntidadeEmUsoException(msg);
