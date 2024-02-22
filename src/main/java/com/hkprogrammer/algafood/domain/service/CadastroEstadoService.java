90d5cba96f689bdb5e331b7393aef57882e89010
Henrick <ricknogueira1231@gmail.com>
2024-02-22 14:23:08 -0300

    8.10. Afinando a granularidade e definindo a hierarquia das exceptions de negócios

--- src/main/java/com/hkprogrammer/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/hkprogrammer/algafood/domain/service/CadastroEstadoService.java
@@ -6,44 +6,36 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.hkprogrammer.algafood.domain.exception.EntidadeEmUsoException;
-import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.hkprogrammer.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.hkprogrammer.algafood.domain.models.Estado;
 import com.hkprogrammer.algafood.domain.repository.EstadoRepository;
 
 @Service
 public class CadastroEstadoService {
 
-	private static final String MSG_ESTADO_EM_USO 
-		= "Estado de código %d não pode ser removido, pois está em uso";
-	
-	private static final String MSG_ESTADO_NAO_ENCONTRADO 
-		= "Não existe um cadastro de estado com código %d";
-	
+	private static final String MSG_ESTADO_EM_USO = "Estado de código %d não pode ser removido, pois está em uso";
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	public Estado salvar(Estado estado) {
 		return estadoRepository.save(estado);
 	}
-	
+
 	public void excluir(Long estadoId) {
 		try {
 			estadoRepository.deleteById(estadoId);
-			
+
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-				String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId));
-		
+			throw new EstadoNaoEncontradoException(estadoId);
+
 		} catch (DataIntegrityViolationException e) {
-			throw new EntidadeEmUsoException(
-				String.format(MSG_ESTADO_EM_USO, estadoId));
+			throw new EntidadeEmUsoException(String.format(MSG_ESTADO_EM_USO, estadoId));
 		}
 	}
 
 	public Estado buscarOuFalhar(Long estadoId) {
-		return estadoRepository.findById(estadoId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
+		return estadoRepository.findById(estadoId).orElseThrow(() -> new EstadoNaoEncontradoException(estadoId));
 	}
-	
+
 }
