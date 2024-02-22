9a822774538e12553004bee7ae970b844a07a033
Henrick <ricknogueira1231@gmail.com>
2024-02-22 14:09:31 -0300

    8.9. Desafio: usando a exception NegocioException

--- src/main/java/com/hkprogrammer/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/hkprogrammer/algafood/api/controller/RestauranteController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.hkprogrammer.algafood.domain.exception.NegocioException;
 import com.hkprogrammer.algafood.domain.models.Restaurante;
 import com.hkprogrammer.algafood.domain.repository.RestauranteRepository;
 import com.hkprogrammer.algafood.domain.service.CadastroRestauranteService;
@@ -43,57 +45,29 @@ public class RestauranteController {
 		return cadastroRestaurante.buscarOuFalhar(restauranteId);
 	}
 	
-//	@PostMapping
-//	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
-//		try {
-//			restaurante = cadastroRestaurante.salvar(restaurante);
-//			
-//			return ResponseEntity.status(HttpStatus.CREATED)
-//					.body(restaurante);
-//		} catch (EntidadeNaoEncontradaException e) {
-//			return ResponseEntity.badRequest()
-//					.body(e.getMessage());
-//		}
-//	}
-	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Restaurante adicionar(@RequestBody Restaurante restaurante) {
-		return cadastroRestaurante.salvar(restaurante);
+	    try {
+	        return cadastroRestaurante.salvar(restaurante);
+	    } catch (EntidadeNaoEncontradaException e) {
+	        throw new NegocioException(e.getMessage());
+	    }
 	}
-	
-//	@PutMapping("/{restauranteId}")
-//	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId,
-//			@RequestBody Restaurante restaurante) {
-//		try {
-//			Restaurante restauranteAtual = restauranteRepository
-//					.findById(restauranteId).orElse(null);
-//			
-//			if (restauranteAtual != null) {
-//				BeanUtils.copyProperties(restaurante, restauranteAtual, 
-//						"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-//				
-//				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
-//				return ResponseEntity.ok(restauranteAtual);
-//			}
-//			
-//			return ResponseEntity.notFound().build();
-//		
-//		} catch (EntidadeNaoEncontradaException e) {
-//			return ResponseEntity.badRequest()
-//					.body(e.getMessage());
-//		}
-//	}
 	
 	@PutMapping("/{restauranteId}")
 	public Restaurante atualizar(@PathVariable Long restauranteId,
-			@RequestBody Restaurante restaurante) {
-		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
-		
-		BeanUtils.copyProperties(restaurante, restauranteAtual, 
-				"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-		
-		return cadastroRestaurante.salvar(restauranteAtual);
+	        @RequestBody Restaurante restaurante) {
+	    Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+	    
+	    BeanUtils.copyProperties(restaurante, restauranteAtual, 
+	            "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+	    
+	    try {
+	        return cadastroRestaurante.salvar(restauranteAtual);
+	    } catch (EntidadeNaoEncontradaException e) {
+	        throw new NegocioException(e.getMessage());
+	    }
 	}
 	
 	@PatchMapping("/{restauranteId}")
