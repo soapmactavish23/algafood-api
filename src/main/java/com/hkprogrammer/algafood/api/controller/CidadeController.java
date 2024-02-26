7df653d32e46973bd9bfbaa54a3b29d92fe42424
Henrick <ricknogueira1231@gmail.com>
2024-02-26 10:48:02 -0300

    8.13. Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/hkprogrammer/algafood/api/controller/CidadeController.java
+++ src/main/java/com/hkprogrammer/algafood/api/controller/CidadeController.java
@@ -1,14 +1,11 @@
 package com.hkprogrammer.algafood.api.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hkprogrammer.algafood.api.exception.Problema;
-import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.hkprogrammer.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.hkprogrammer.algafood.domain.exception.NegocioException;
 import com.hkprogrammer.algafood.domain.models.Cidade;
@@ -73,12 +68,6 @@ public class CidadeController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
-	}
-
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntitidadeNaoEncontrada(EntidadeNaoEncontradaException e) {
-		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
 	}
 
 }
---
+++ src/main/java/com/hkprogrammer/algafood/api/exception/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.hkprogrammer.algafood.api.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.hkprogrammer.algafood.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntitidadeNaoEncontrada(EntidadeNaoEncontradaException e) {
+		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	}
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	}
+
+	@ExceptionHandler(HttpMediaTypeException.class)
+	public ResponseEntity<?> tratarHttpMediaTypeException() {
+		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem("O tipo de mídia não é aceito")
+				.build();
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
+	}
+
+}
