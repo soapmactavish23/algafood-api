1a29cf1a94a853455e03035d0f7ca5e36f7c0138
soapmactavish23 <ricknogueira1231@gmail.com>
2024-03-01 20:49:26 -0300

    9.4. Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/com/hkprogrammer/algafood/api/exception/ProblemType.java
+++ src/main/java/com/hkprogrammer/algafood/api/exception/ProblemType.java
@@ -11,6 +11,7 @@ public enum ProblemType {
 	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");       
 	
 	private String title;
