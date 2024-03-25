a0eb3de04502549d3636406c4061fdf985206dc2
soapmactavish23 <ricknogueira1231@gmail.com>
2024-03-25 00:26:08 -0300

    11.12. Refatorando e criando um assembler de DTO

--- src/main/java/com/hkprogrammer/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/hkprogrammer/algafood/api/controller/RestauranteController.java
@@ -1,7 +1,6 @@
 package com.hkprogrammer.algafood.api.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hkprogrammer.algafood.api.model.CozinhaModel;
+import com.hkprogrammer.algafood.api.assembler.RestauranteModelAssembler;
 import com.hkprogrammer.algafood.api.model.RestauranteModel;
 import com.hkprogrammer.algafood.api.model.input.RestauranteInput;
 import com.hkprogrammer.algafood.domain.exception.CozinhaNaoEncontradaException;
@@ -37,19 +36,19 @@ public class RestauranteController {
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
 
-//	@Autowired
-//	private SmartValidator validator;
+	@Autowired
+	private RestauranteModelAssembler restauranteModelAssembler;
 
 	@GetMapping
 	public List<RestauranteModel> listar() {
-		return toCollectionModel(restauranteRepository.findAll());
+		return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
 	}
 
 	@GetMapping("/{restauranteId}")
 	public RestauranteModel buscar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-		RestauranteModel restauranteModel = toModel(restaurante);
+		RestauranteModel restauranteModel = restauranteModelAssembler.toModel(restaurante);
 
 		return restauranteModel;
 	}
@@ -60,7 +59,7 @@ public class RestauranteController {
 		try {
 			Restaurante restaurante = toDomainObject(restauranteInput);
 			
-			return toModel(cadastroRestaurante.salvar(restaurante));
+			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restaurante));
 		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
@@ -76,76 +75,10 @@ public class RestauranteController {
 				"produtos");
 
 		try {
-			return toModel(cadastroRestaurante.salvar(restauranteAtual));
+			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
 		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
-	}
-
-//	@PatchMapping("/{restauranteId}")
-//	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
-//			HttpServletRequest request) {
-//		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
-//
-//		merge(campos, restauranteAtual, request);
-//		validate(restauranteAtual, "restaurante");
-//
-//		return atualizar(restauranteId, restauranteAtual);
-//	}
-//
-//	private void validate(Restaurante restaurante, String objectName) {
-//		BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(restaurante, objectName);
-//
-//		validator.validate(restaurante, bindingResult);
-//
-//		if (bindingResult.hasErrors()) {
-//			throw new ValidacaoException(bindingResult);
-//		}
-//	}
-//
-//	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
-//
-//		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
-//
-//		try {
-//			ObjectMapper objectMapper = new ObjectMapper();
-//
-//			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
-//			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-//
-//			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-//
-//			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-//				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-//				field.setAccessible(true);
-//
-//				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-//
-//				ReflectionUtils.setField(field, restauranteDestino, novoValor);
-//			});
-//		} catch (IllegalArgumentException e) {
-//			Throwable rootCause = ExceptionUtils.getRootCause(e);
-//			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
-//		}
-//	}
-
-	private RestauranteModel toModel(Restaurante restaurante) {
-
-		CozinhaModel cozinhaModel = new CozinhaModel();
-		cozinhaModel.setId(restaurante.getCozinha().getId());
-		cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-		RestauranteModel restauranteModel = new RestauranteModel();
-		restauranteModel.setId(restaurante.getId());
-		restauranteModel.setNome(restaurante.getNome());
-		restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteModel.setCozinha(cozinhaModel);
-
-		return restauranteModel;
-	}
-
-	private List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
-		return restaurantes.stream().map(restaurante -> toModel(restaurante)).collect(Collectors.toList());
 	}
 
 	private Restaurante toDomainObject(RestauranteInput restauranteInput) {
--- src/main/java/com/hkprogrammer/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/hkprogrammer/algafood/core/jackson/JacksonMixinModule.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.hkprogrammer.algafood.api.model.mixin.CidadeMixin;
 import com.hkprogrammer.algafood.api.model.mixin.CozinhaMixin;
-import com.hkprogrammer.algafood.api.model.mixin.RestauranteMixin;
 import com.hkprogrammer.algafood.domain.models.Cidade;
 import com.hkprogrammer.algafood.domain.models.Cozinha;
-import com.hkprogrammer.algafood.domain.models.Restaurante;
 
 @Component
 public class JacksonMixinModule extends SimpleModule {
@@ -16,7 +14,6 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 	
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 	    setMixInAnnotation(Cidade.class, CidadeMixin.class);
 	    setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 	}
