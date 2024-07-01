42f75cb6c1f9817987cb08d4a92557a8e8f60907
soapmactavish23 <ricknogueira1231@gmail.com>
2024-07-01 17:29:17 -0300

    13.3. Limitando os campos retornados pela API com Squiggly

---
+++ src/main/java/com/hkprogrammer/algafood/core/squiggly/SquigglyConfig.java
@@ -0,0 +1,31 @@
+package com.hkprogrammer.algafood.core.squiggly;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.web.RequestSquigglyContextProvider;
+import com.github.bohnman.squiggly.web.SquigglyRequestFilter;
+
+@Configuration
+public class SquigglyConfig {
+
+	public FilterRegistrationBean<SquigglyRequestFilter> squigglyRequestFilter(ObjectMapper objectMapper) {
+		
+		Squiggly.init(objectMapper, new RequestSquigglyContextProvider("campos", null));
+		
+		var urlPatters = Arrays.asList("/pedidos/*", "/restaurantes/*");
+		
+		var filterRegistration = new FilterRegistrationBean<SquigglyRequestFilter>();
+		filterRegistration.setFilter(new SquigglyRequestFilter());
+		filterRegistration.setOrder(1);
+		filterRegistration.setUrlPatterns(urlPatters);
+		
+		return filterRegistration;
+		
+	}
+	
+}
