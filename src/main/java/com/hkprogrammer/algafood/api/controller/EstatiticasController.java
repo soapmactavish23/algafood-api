8313053503a11f1d803238f10743b647eda8d719
soapmactavish23 <ricknogueira1231@gmail.com>
2024-08-26 11:00:34 -0300

    13.16. Tratando time offset na agregação de vendas diárias por data

--- src/main/java/com/hkprogrammer/algafood/api/controller/EstatiticasController.java
+++ src/main/java/com/hkprogrammer/algafood/api/controller/EstatiticasController.java
@@ -6,6 +6,7 @@ import com.hkprogrammer.algafood.infraestructure.service.VendaQueryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +19,10 @@ public class EstatiticasController {
     private VendaQueryServiceImpl vendaQueryService;
 
     @GetMapping("/vendas-diarias")
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-        return vendaQueryService.consultarVendasDiarias(filtro);
+    public List<VendaDiaria> consultarVendasDiarias(
+            VendaDiariaFilter filtro,
+            @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+        return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
     }
 
 }
