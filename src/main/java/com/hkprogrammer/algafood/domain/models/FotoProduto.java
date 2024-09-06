4b56ee88dbf7d7448c7224b3a42f804271d8b3c1
soapmactavish23 <ricknogueira1231@gmail.com>
2024-09-06 13:55:45 -0300

    14.5. Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/hkprogrammer/algafood/domain/models/FotoProduto.java
@@ -0,0 +1,29 @@
+package com.hkprogrammer.algafood.domain.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FotoProduto {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "produto_id")
+    private Long id;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    private Produto produto;
+
+    private String nomeArquivo;
+
+    private String descricao;
+
+    private String contentType;
+
+    private Long tamanho;
+}
