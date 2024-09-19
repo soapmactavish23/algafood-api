2ce2370f26b63c3ecc8775c2441362a3ef54a39f
soapmactavish23 <ricknogueira1231@gmail.com>
2024-09-19 11:34:49 -0300

    14.26. Selecionando a implementação do serviço de storage de fotos[

--- src/main/java/com/hkprogrammer/algafood/infraestructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/hkprogrammer/algafood/infraestructure/service/storage/S3FotoStorageService.java
@@ -1,10 +1,8 @@
 package com.hkprogrammer.algafood.infraestructure.service.storage;
 
-import java.io.InputStream;
 import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
@@ -14,7 +12,6 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.hkprogrammer.algafood.core.storage.StorageProperties;
 import com.hkprogrammer.algafood.domain.service.FotoStorageService;
 
-@Service
 public class S3FotoStorageService implements FotoStorageService {
 
 	@Autowired
