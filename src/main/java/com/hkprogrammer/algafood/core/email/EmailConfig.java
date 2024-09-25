5a3653e8f493b3dd2468936364ea61a445088e35
soapmactavish23 <ricknogueira1231@gmail.com>
2024-09-25 10:32:14 -0300

    15.8. Desafio: implementando serviço de envio de e-mail fake

---
+++ src/main/java/com/hkprogrammer/algafood/core/email/EmailConfig.java
@@ -0,0 +1,28 @@
+package com.hkprogrammer.algafood.core.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hkprogrammer.algafood.domain.service.EnvioEmailService;
+import com.hkprogrammer.algafood.infraestructure.service.email.FakeEnvioEmailService;
+import com.hkprogrammer.algafood.infraestructure.service.email.SmtpEnvioEmailService;
+
+@Configuration
+public class EmailConfig {
+
+    @Autowired
+    private EmailProperties emailProperties;
+
+    @Bean
+    public EnvioEmailService envioEmailService() {       
+        switch (emailProperties.getImpl()) {
+            case FAKE:
+                return new FakeEnvioEmailService();
+            case SMTP:
+                return new SmtpEnvioEmailService();
+            default:
+                return null;
+        }
+    }
+}
