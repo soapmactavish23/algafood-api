28beeb95e32abb2986af342531ab787414aa6a5f
soapmactavish23 <ricknogueira1231@gmail.com>
2024-11-10 17:50:59 -0300

    19.25. Desafio: adicionando links condicionais no recurso de restaurante

--- src/main/java/com/hkprogrammer/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/hkprogrammer/algafood/api/controller/RestauranteController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +27,6 @@ import com.hkprogrammer.algafood.api.assembler.RestauranteInputDisassembler;
 import com.hkprogrammer.algafood.api.assembler.RestauranteModelAssembler;
 import com.hkprogrammer.algafood.api.model.RestauranteModel;
 import com.hkprogrammer.algafood.api.model.input.RestauranteInput;
-import com.hkprogrammer.algafood.api.model.view.RestauranteView;
 import com.hkprogrammer.algafood.domain.exception.CidadeNaoEncontradaException;
 import com.hkprogrammer.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.hkprogrammer.algafood.domain.exception.NegocioException;
