fafe8c117a1b0602d183883a752bbee80daf9ce4
soapmactavish23 <ricknogueira1231@gmail.com>
2024-12-27 16:08:36 -0300

    23.17. Obtendo usuário autenticado no Resource Server

--- src/main/java/com/hkprogrammer/algafood/api/v1/controller/PedidoController.java
+++ src/main/java/com/hkprogrammer/algafood/api/v1/controller/PedidoController.java
@@ -1,23 +1,5 @@
 package com.hkprogrammer.algafood.api.v1.controller;
 
-import javax.validation.Valid;
-
-import com.hkprogrammer.algafood.core.data.PageWrapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.google.common.collect.ImmutableMap;
 import com.hkprogrammer.algafood.api.v1.assembler.PedidoInputDisassembler;
 import com.hkprogrammer.algafood.api.v1.assembler.PedidoModelAssembler;
@@ -25,17 +7,28 @@ import com.hkprogrammer.algafood.api.v1.assembler.PedidoResumoModelAssembler;
 import com.hkprogrammer.algafood.api.v1.model.PedidoModel;
 import com.hkprogrammer.algafood.api.v1.model.PedidoResumoModel;
 import com.hkprogrammer.algafood.api.v1.model.input.PedidoInput;
+import com.hkprogrammer.algafood.core.data.PageWrapper;
 import com.hkprogrammer.algafood.core.data.PageableTranslator;
+import com.hkprogrammer.algafood.core.security.AlgaSecurity;
 import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.hkprogrammer.algafood.domain.exception.NegocioException;
+import com.hkprogrammer.algafood.domain.filter.PedidoFilter;
 import com.hkprogrammer.algafood.domain.models.Pedido;
 import com.hkprogrammer.algafood.domain.models.Usuario;
 import com.hkprogrammer.algafood.domain.repository.PedidoRepository;
-import com.hkprogrammer.algafood.domain.filter.PedidoFilter;
 import com.hkprogrammer.algafood.domain.service.EmissaoPedidoService;
 import com.hkprogrammer.algafood.infraestructure.repository.spec.PedidoSpecs;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/v1/pedidos")
@@ -56,6 +49,9 @@ public class PedidoController {
 
 	@Autowired
 	private PedidoInputDisassembler pedidoInputDisassembler;
+
+	@Autowired
+	private AlgaSecurity algaSecurity;
 
 	private PagedResourcesAssembler<Pedido> pagedResourcesAssembler;
 
@@ -85,9 +81,9 @@ public class PedidoController {
 		try {
 			Pedido novoPedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
 
-			// TODO pegar usuário autenticado
+			// TODO: pegar usuário autenticado
 			novoPedido.setCliente(new Usuario());
-			novoPedido.getCliente().setId(1L);
+			novoPedido.getCliente().setId(algaSecurity.getUsuarioId());
 
 			novoPedido = emissaoPedido.emitir(novoPedido);
 
