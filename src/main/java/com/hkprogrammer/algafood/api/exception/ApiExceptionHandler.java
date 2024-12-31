e5e6d9f0693cd0367c94632d30c3ef8dc700b2be
soapmactavish23 <ricknogueira1231@gmail.com>
2024-12-31 14:05:52 -0300

    23.22. Desafio: tratando AccessDeniedException no ExceptionHandler

--- src/main/java/com/hkprogrammer/algafood/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/hkprogrammer/algafood/api/exception/ApiExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.hkprogrammer.algafood.api.exception;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
+import com.hkprogrammer.algafood.core.validation.ValidacaoException;
+import com.hkprogrammer.algafood.domain.exception.EntidadeEmUsoException;
+import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.hkprogrammer.algafood.domain.exception.NegocioException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +28,10 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.fasterxml.jackson.databind.JsonMappingException.Reference;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import com.fasterxml.jackson.databind.exc.PropertyBindingException;
-import com.hkprogrammer.algafood.core.validation.ValidacaoException;
-import com.hkprogrammer.algafood.domain.exception.EntidadeEmUsoException;
-import com.hkprogrammer.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.hkprogrammer.algafood.domain.exception.NegocioException;
+import java.nio.file.AccessDeniedException;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -266,5 +266,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler({ ValidacaoException.class })
 	public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
 		return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<?> handleEntidadeNaoEncontrada(AccessDeniedException ex, WebRequest request) {
+
+		HttpStatus status = HttpStatus.FORBIDDEN;
+		ProblemType problemType = ProblemType.ACESSO_NEGADO;
+		String detail = ex.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage("Você não possui permissão para executar essa operação.")
+				.build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 }
--- src/main/java/com/hkprogrammer/algafood/api/exception/ProblemType.java
+++ src/main/java/com/hkprogrammer/algafood/api/exception/ProblemType.java
@@ -12,7 +12,8 @@ public enum ProblemType {
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
 	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");       
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	ACESSO_NEGADO("/acesso-negado", "Acesso negado");
 	
 	private String title;
 	private String uri;
