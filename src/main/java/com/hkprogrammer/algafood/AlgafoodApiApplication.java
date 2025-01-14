6ee1cca80f4330d139034c8f6ae31a77c1672dfb
Henrick <ricknogueira1231@gmail.com>
2025-01-14 11:48:32 -0300

    23.46. Externalizando o KeyStore: criando um ProtocolResolver para Base64

--- src/main/java/com/hkprogrammer/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/hkprogrammer/algafood/AlgafoodApiApplication.java
@@ -1,12 +1,12 @@
 package com.hkprogrammer.algafood;
 
-import java.util.TimeZone;
-
+import com.hkprogrammer.algafood.core.io.Base64ProtocolResolver;
+import com.hkprogrammer.algafood.infraestructure.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.hkprogrammer.algafood.infraestructure.repository.CustomJpaRepositoryImpl;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
@@ -14,7 +14,9 @@ public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		SpringApplication.run(AlgafoodApiApplication.class, args);
+		var app = new SpringApplication(AlgafoodApiApplication.class);
+		app.addListeners(new Base64ProtocolResolver());
+		app.run(args);
 	}
 
 }
