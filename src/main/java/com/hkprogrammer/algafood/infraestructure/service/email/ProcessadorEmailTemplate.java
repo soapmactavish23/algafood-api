a0a838e45c21ca7d2d4fdbc33d06ceecbf0ce33a
Henrick <ricknogueira1231@gmail.com>
2025-01-20 21:26:45 -0300

    25.3. Dependência de JavaMailSender não satisfeita: melhorando o uso da herança

---
+++ src/main/java/com/hkprogrammer/algafood/infraestructure/service/email/ProcessadorEmailTemplate.java
@@ -0,0 +1,26 @@
+package com.hkprogrammer.algafood.infraestructure.service.email;
+
+import com.hkprogrammer.algafood.domain.service.EnvioEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+@Component
+public class ProcessadorEmailTemplate {
+
+    @Autowired
+    private Configuration freemarkerConfig;
+
+    protected String processarTemplate(EnvioEmailService.Mensagem mensagem) {
+        try {
+            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
+        } catch (Exception e) {
+            throw new EmailException("Não foi possível montar o template do e-mail", e);
+        }
+    }
+
+}
--- src/main/java/com/hkprogrammer/algafood/infraestructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/hkprogrammer/algafood/infraestructure/service/email/SmtpEnvioEmailService.java
@@ -2,12 +2,9 @@ package com.hkprogrammer.algafood.infraestructure.service.email;
 
 import com.hkprogrammer.algafood.core.email.EmailProperties;
 import com.hkprogrammer.algafood.domain.service.EnvioEmailService;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -19,8 +16,9 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	
 	@Autowired
 	private EmailProperties emailProperties;
-	
-	private Configuration freemarkerConfig;
+
+	@Autowired
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
@@ -33,18 +31,8 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	    }
 	}
 	
-	protected String processarTemplate(Mensagem mensagem) {
-		try {
-			Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
-			
-			return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
-		}
-	}
-	
 	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-	    String corpo = processarTemplate(mensagem);
+	    String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 	    
 	    MimeMessage mimeMessage = mailSander.createMimeMessage();
 	    
