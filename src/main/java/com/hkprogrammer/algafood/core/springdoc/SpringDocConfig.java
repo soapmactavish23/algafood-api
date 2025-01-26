0abddc5084ebb2e22b0355470ed64aa2a106b939
Henrick <ricknogueira1231@gmail.com>
2025-01-26 20:30:09 -0300

    27.3. Configuração inicial do Authorization Server com Access Token opaco

--- src/main/java/com/hkprogrammer/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/hkprogrammer/algafood/core/springdoc/SpringDocConfig.java
@@ -9,6 +9,9 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,6 +41,23 @@ public class SpringDocConfig {
                 ).externalDocs(new ExternalDocumentation()
                         .description("AlgaWorks")
                         .url("https://algaworks.com"));
+    }
+
+    @Bean
+    public OpenApiCustomiser openApiCustomiser() {
+        return openApi -> {
+            openApi.getPaths()
+                    .values()
+                    .stream()
+                    .flatMap(pathItem -> pathItem.readOperations().stream())
+                    .forEach(operation -> {
+                        ApiResponses responses = operation.getResponses();
+
+                        ApiResponse apiResponseErroInterno = new ApiResponse().description("Erro interno no servidor");
+
+                        responses.addApiResponse("500", apiResponseErroInterno);
+                    });
+        };
     }
 
 }
